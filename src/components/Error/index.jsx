0699538986018b3a852c5a33afb7138a270a8e75
Oxana Theis <otheis@protonmail.com>
2022-07-04 13:08:27 +0200

    version avec error page media query

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -3,12 +3,61 @@ import error from "../../assets/error.svg";
 import styled from "styled-components";
 
 const Image = styled.img`
-display:flex;
-  width: 80%;
- 
+  display: flex;
+  width: 50%;
+  margin-top: 60px;
+
+  @media all and (min-width: 375px) and (max-width: 600px) {
+    width: 70%;
+  }
 `;
 const Wrapper = styled.div`
   display: flex;
+  align-items: center;
+
+  @media all and (min-width: 375px) and (max-width: 890px) {
+    flex-direction: column-reverse;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+ 
+   margin-left:40px;
+
+
+  
+
+    @media all and (min-width: 375px) and (max-width: 890px) {
+      margin-left: 20px;
+    }
+
+    p {
+      font-size: 1.7em;
+      color:#282D30;
+
+    }
+    a {
+      text-decoration: none;
+      background: #ff0101;
+      width: 110px;
+      height: 40px;
+      border-radius: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+
+
+      @media all and (min-width: 375px) and (max-width: 890px) {
+        position:absolute;
+        bottom:40px;
+        align-self:center;
+      }
+
+    }
+   
   
 `;
 function Error() {
@@ -16,10 +65,15 @@ function Error() {
     <Wrapper>
       <Image src={error} alt=""></Image>
       <div>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <p>
+          {" "}
+          {<span style={{ color: "#FF0101", fontWeight: "bold", fontSize: "1.6em" }}>Oups!</span>}
+          <br /> <br />
+          La page que vous demandez n'existe pas.
+        </p>
 
         <Link to="/" className="title-link">
-          Retourner sur la page d'accueil
+          ACCUEIL
         </Link>
       </div>
     </Wrapper>

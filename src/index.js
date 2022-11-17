41715f73ec15df409fb938013082b77038e59d37
Oxana Theis <otheis@protonmail.com>
2022-11-17 11:55:05 +0100

    test deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 import Login from "./pages/Login/Login";
 import DashBoard from "./pages/DashBoard/DashBoard";
 import Error from "./components/Error/Error";
-import { BrowserRouter as Router, Routes, Route,HashRouter,HashHistory } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router history={HashHistory} >
+    <Router>
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/user/:id" element={<DashBoard />}></Route>
